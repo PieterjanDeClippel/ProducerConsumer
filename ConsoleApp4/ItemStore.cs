18ecabec4723bfcd492ba7d78c1435d738e39fa6
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-03-07 13:32:52 +0100

    Add project files.

---
+++ ConsoleApp4/ItemStore.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp4;
+
+public class ItemStore
+{
+    private readonly Item[] items = Enumerable.Range(0, 5000).Select(i => new Item { Id = i, Name = $"Item {i}" }).ToArray();
+
+    public async Task<Item[]> GetItems(int offset, int count)
+    {
+        await Task.Delay(10000);
+        return items.Skip(offset).Take(count).ToArray();
+    }
+}
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp4;
+using System.Threading.Channels;
+
+var channel = Channel.CreateUnbounded<Item>();
+
+var producer = Task.Run(async () =>
+{
+    for (int i = 1; i <= 20; i++)
+    {
+        Console.WriteLine($"Producing {i}");
+        // Send number to channel
+        await channel.Writer.WriteAsync(new Item
+        {
+            Id = i,
+            Name = $"Item {i}",
+        });
+        await Task.Delay(5000); // Simulate work
+        Console.WriteLine($"Produced {i}");
+    }
+});
+
+var consumers = Enumerable.Range(0, 3).Select(i => Task.Run(async () =>
+{
+   await foreach (var item in channel.Reader.ReadAllAsync())
+   {
+       Console.WriteLine($"Consumer {i} processing {item.Name}");
+       await Task.Delay(20000); // Simulate processing
+       Console.WriteLine($"Consumer {i} processed {item.Name}");
+   }
+})).ToList();
+
+await producer;
+channel.Writer.Complete(); // Signal consumers that no more items will be added
+await Task.WhenAll(consumers);
