2032ac59b6686ef22db98051aa33555b49ba043a
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-03-09 14:52:49 +0100

    Temp commit

---
+++ ConsoleApp4/ChannelEx.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp4;
+
+public static class ChannelEx
+{
+    public static void Pipeline<T>(this Channel<T> channel)
+    {
+
+    }
+}
+
+public class Pipeline
+{
+    public static void Create<T>()
+    {
+
+    }
+}
