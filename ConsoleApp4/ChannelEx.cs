cc91bc1833f5f98e0d2434ea9f63888241ba5d9a
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-03-09 15:20:49 +0100

    Cleanup

--- ConsoleApp4/ChannelEx.cs
+++ ConsoleApp4/ChannelEx.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
 
 namespace ConsoleApp4;
 
@@ -10,10 +11,32 @@ public static class ChannelEx
     }
 }
 
-public class Pipeline
+public class Pipeline<T>
 {
-    public static void Create<T>()
+    public static Pipeline<T> Create(Func<int, int, Task<bool>> action, int consumerCount = 1)
     {
-
+        var result = new Pipeline<T>
+        {
+            tasks = Enumerable.Range(0, consumerCount)
+                .Select(consumerId => Task.Run(async () =>
+                {
+                    var hasMore = true;
+                    var i = 0;
+                    do
+                    {
+                        hasMore = await action(i, consumerId);
+                        i++;
+                    }
+                    while (hasMore);
+                }))
+                .ToArray(),
+        };
+        return result;
     }
+
+    private readonly Channel<T> channel = Channel.CreateUnbounded<T>();
+    private IEnumerable<Task> tasks = [];
+
+    public TaskAwaiter GetAwaiter()
+        => Task.WhenAll(tasks).GetAwaiter();
 }
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleApp4;
 using System.Threading.Channels;
+using static Confluent.Kafka.ConfigPropertyNames;
 
 
 var store = new ItemStore();
@@ -8,100 +9,73 @@ var store = new ItemStore();
 var channelA = Channel.CreateUnbounded<ItemsResponse>();
 var channelB = Channel.CreateUnbounded<ItemsResponse>();
 
-var producerA = Enumerable.Range(0, 1)
-    .Select(_ => Task.Run(async () =>
-    {
-        var hasMore = true;
-        var i = 0;
-        do
-        {
-            ConsoleEx.WriteLine($"[A] Fetching page {i}");
-            var items = await store.GetItems(i * 25, 25);
-            await channelA.Writer.WriteAsync(items);
-            ConsoleEx.WriteLine($"""
-                [A] Fetched page {i}
-                    {string.Join(',', items.Items.Select(x => x.Name))}
-                """);
-            
-            hasMore = items.HasMore;
-            i++;
-        }
-        while (hasMore);
-    }));
+var pipelineA = Pipeline<ItemsResponse>.Create(async (pageNumber, consumerId) =>
+{
+    ConsoleEx.WriteLine($"[A] Fetching page {pageNumber}");
+    var items = await store.GetItems(pageNumber * 25, 25);
+    await channelA.Writer.WriteAsync(items);
+    ConsoleEx.WriteLine($"""
+        [A] Fetched page {pageNumber}
+            {string.Join(',', items.Items.Select(x => x.Name))}
+        """);
 
-var consumersA = Enumerable.Range(0, 3)
-    .Select(consumerId => Task.Run(async () =>
+    return items.HasMore;
+});
+
+var pipelineB = Pipeline<ItemsResponse>.Create(async (pageNumber, consumerId) =>
+{
+    await foreach (var page in channelA.Reader.ReadAllAsync())
     {
-        var hasMore = true;
-        var i = 0;
-        do
+        ConsoleEx.WriteLine($"[B] Reversing {page.PageStart} to {page.PageEnd} (Consumer {consumerId})");
+        await Task.Delay(5000); // Simulate processing
+        var reversed = new ItemsResponse
         {
-            await foreach (var page in channelA.Reader.ReadAllAsync())
+            PageStart = page.PageStart,
+            HasMore = page.HasMore,
+            Items = page.Items.Select(x => new Item
             {
-                ConsoleEx.WriteLine($"[B] Reversing {page.PageStart} to {page.PageEnd} (Consumer {consumerId})");
-                await Task.Delay(5000); // Simulate processing
-                var reversed = new ItemsResponse
-                {
-                    PageStart = page.PageStart,
-                    HasMore = page.HasMore,
-                    Items = page.Items.Select(x => new Item
-                    {
-                        Id = x.Id,
-                        Name = new string(x.Name.Reverse().ToArray()),
-                    }).ToArray(),
-                };
-                await channelB.Writer.WriteAsync(reversed);
-                ConsoleEx.WriteLine($"""
-                    [B] Reversed {reversed.PageStart} to {reversed.PageEnd} (Consumer {consumerId})
-                        {string.Join(',', reversed.Items.Select(x => x.Name))}
-                    """);
-            }
+                Id = x.Id,
+                Name = new string(x.Name.Reverse().ToArray()),
+            }).ToArray(),
+        };
+        await channelB.Writer.WriteAsync(reversed);
+        ConsoleEx.WriteLine($"""
+            [B] Reversed {reversed.PageStart} to {reversed.PageEnd} (Consumer {consumerId})
+                {string.Join(',', reversed.Items.Select(x => x.Name))}
+            """);
 
-            hasMore = false;
-            i++;
-        }
-        while (hasMore);
-    }))
-    .ToList();
+    }
+    return false;
+}, 3);
 
-
-var consumersB = Enumerable.Range(0, 3)
-    .Select(consumerId => Task.Run(async () =>
+var pipelineC = Pipeline<ItemsResponse>.Create(async (pageNumber, consumerId) =>
+{
+    await foreach (var page in channelB.Reader.ReadAllAsync())
     {
-        var hasMore = true;
-        var i = 0;
-        do
+        ConsoleEx.WriteLine($"[C] Capsing {page.PageStart} to {page.PageEnd} (Consumer {consumerId})");
+        await Task.Delay(6000); // Simulate processing
+        var capsed = new ItemsResponse
         {
-            await foreach (var page in channelB.Reader.ReadAllAsync())
+            PageStart = page.PageStart,
+            HasMore = page.HasMore,
+            Items = page.Items.Select(x => new Item
             {
-                ConsoleEx.WriteLine($"[C] Capsing {page.PageStart} to {page.PageEnd} (Consumer {consumerId})");
-                await Task.Delay(6000); // Simulate processing
-                var capsed = new ItemsResponse
-                {
-                    PageStart = page.PageStart,
-                    HasMore = page.HasMore,
-                    Items = page.Items.Select(x => new Item
-                    {
-                        Id = x.Id,
-                        Name = new string(x.Name.ToUpper()),
-                    }).ToArray(),
-                };
-                ConsoleEx.WriteLine($"""
-                    [C] Capsed {capsed.PageStart} to {capsed.PageEnd} (Consumer {consumerId})
-                        {string.Join(',', capsed.Items.Select(x => x.Name))}
-                    """);
-            }
+                Id = x.Id,
+                Name = new string(x.Name.ToUpper()),
+            }).ToArray(),
+        };
+        ConsoleEx.WriteLine($"""
+            [C] Capsed {capsed.PageStart} to {capsed.PageEnd} (Consumer {consumerId})
+                {string.Join(',', capsed.Items.Select(x => x.Name))}
+            """);
 
-            hasMore = false;
-            i++;
-        }
-        while (hasMore);
-    }))
-    .ToList();
+    }
+    return false;
+}, 3);
 
 
-await Task.WhenAll(producerA);
+await pipelineA;
 channelA.Writer.Complete(); // Signal consumers that no more items will be added
-await Task.WhenAll(consumersA);
+await pipelineB;
 channelB.Writer.Complete(); // Signal consumers that no more items will be added
-await Task.WhenAll(consumersB);
+await pipelineC;
